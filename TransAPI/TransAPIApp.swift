7d5ffb4c4e3844d475c9205244d13725ac913020
Danny Shen <shenfive@gmail.com>
2025-06-23 12:27:34 +0800

    Initial Commit

---
+++ TransAPI/TransAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransAPIApp.swift
+//  TransAPI
+//
+//  Created by DannyShen on 2025/6/23.
+//
+
+import SwiftUI
+
+@main
+struct TransAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
